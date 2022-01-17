23550b52688caa86bce03a65c15c55a25c0b2578
Thorsten Klöhn <thorstenkloehn@gmail.com>
2022-01-17 03:27:36 +0100

    Test

--- Programmiersprachen/01Lektion_Datentypen/Csharpe/Program.cs
+++ Programmiersprachen/01Lektion_Datentypen/Csharpe/Program.cs
@@ -1,7 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Start {
 
-byte variable = 100; //int8
+public Start() {
+    Console.WriteLine("Hallo Leute");
+}
+
+}
+
+
+public class Program {
+
+    public static void Main(string[] args) {
+
+        var start= new Start();
+        
+        byte variable = 100; //int8
 short variable1 = 3000; //int16
 int variable2 = 30000; //int32
 long variable3 = 39999; //int64
@@ -11,4 +23,7 @@ ushort variable5 = 300; //uint16
 uint variable6= 400; //uint32
 ulong variable7=600; //uint64
 
+var zahl = 100;
+    }
 
+}
